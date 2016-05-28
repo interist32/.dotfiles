eea0c448b0d508f3d581102d78afc9f027ab825d
Vladimir Makushev <interist32@gmail.com>
2016-05-28 14:38:25 +0300

    Git config added

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Symlinks
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
 #Package managers, packages, apps
 . "$DOTFILES_DIR/install/brew.sh"
